f8c0321ac80e7aa3451b9142690cb4d1073ceb12
Pedro Lages <pedro.slages2@gmail.com>
2021-04-26 18:48:58 -0300

    first commit

---
+++ lib/reports_generator.ex
@@ -0,0 +1,44 @@
+defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["foods", "users"]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def fetch_higher_cost(report, option) when option in @options do
+    result = Enum.max_by(report[option], fn {_key, value} -> value end)
+    {:ok, result}
+  end
+
+  def fetch_higher_cost(_report, _option),
+    do: {:error, "Invalid option! It has to be 'foods' or 'users'"}
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    foods = Map.put(foods, food_name, foods[food_name] + price)
+    users = Map.put(users, id, users[id] + price)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
+  end
+end
